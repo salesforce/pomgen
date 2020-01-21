83b6e29698cd7aadb24e67eeaa634ec97a1ec715
Simon Toens <stoens@salesforce.com>
2020-01-21 11:12:43 +0900

    Add option to read dependencies from stdin (bazel query output)

--- misc/all_third_party_deps_pom.py
+++ misc/all_third_party_deps_pom.py
@@ -19,10 +19,19 @@ import argparse
 import os
 import sys
 
+IGNORED_DEPENDENCY_PREFIXES = ("@bazel_tools",
+                               "@local_config",
+                               "@local_jdk",
+                               "@remote_coverage_tools",
+                               "@remote_java_tools",
+                              )
+
 def _parse_arguments(args):
-    parser = argparse.ArgumentParser(description="3rdparty Pom Generator")
+    parser = argparse.ArgumentParser(description="Third Party Pom Generator. This script generates a single pom file to stdout, containing 3rdparty dependencies. Dependencies are de-duped and sorted alphabetically by their group and artifact ids. If given no input, this script processes all declared 3rd party dependencies.")
     parser.add_argument("--repo_root", type=str, required=False,
         help="the root of the repository")
+    parser.add_argument("--stdin", required=False, action='store_true',
+        help="reads dependencies from stdin - this is useful to chain bazel query command(s), as input to this script. In this mode, this script will look for dependencies of the form \"@<name>//jar\", one on each line. Some special dependencies (for example @remote), are ignored.")
     return parser.parse_args(args)
 
 class ThirdPartyDepsPomGen(pom.DynamicPomGen):
@@ -32,6 +41,12 @@ class ThirdPartyDepsPomGen(pom.DynamicPomGen):
 
     def _load_additional_dependencies_hook(self):
         return self.dependencies
+
+def _starts_with_ignored_prefix(line):
+    for prefix in IGNORED_DEPENDENCY_PREFIXES:
+        if line.startswith(prefix):
+            return True
+    return False
 
 if __name__ == "__main__":
     args = _parse_arguments(sys.argv[1:])
@@ -46,8 +61,24 @@ if __name__ == "__main__":
                                              artifact_id="all_ext_deps_art",
                                              version="0.0.1-SNAPSHOT")
 
-    all_dependencies = list(workspace.name_to_external_dependencies.values())
-    all_dependencies.sort()
-    pomgen = ThirdPartyDepsPomGen(workspace, artifact_def, all_dependencies, cfg.pom_template)
+    if args.stdin:
+        dep_labels = []
+        dependencies = []
+        for line in sys.stdin:
+            line = line.strip()
+            if len(line) == 0:
+                continue
+            if not line.startswith("@"):
+                continue
+            if _starts_with_ignored_prefix(line):
+                continue
+            dep_labels.append(line)
+        unique_dependencies = set(workspace.parse_dep_labels(dep_labels))
+        dependencies = list(unique_dependencies)
+    else:
+        dependencies = list(workspace.name_to_external_dependencies.values())
+
+    dependencies.sort()
+    pomgen = ThirdPartyDepsPomGen(workspace, artifact_def, dependencies, cfg.pom_template)
     pomgen.process_dependencies()
     print(pomgen.gen())
