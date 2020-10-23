628b5ba300e4a84269e4454f31ce749d4ae1fa51
Simon Toens <stoens@salesforce.com>
2020-10-23 16:27:36 +0900

    Better way to determine whether an artifact is built by bazel (#63) (#64)
    (cherry picked from commit d0a2432edcbf1b2ef2250bc5fb0baac4645fb7aa)

--- common/pomgenmode.py
+++ common/pomgenmode.py
@@ -23,15 +23,19 @@ class PomGenMode:
     The pom generation mode, as specified by the `pom_generation_mode` attribute
     of the `maven_artifact` rule.
     """
-    def __init__(self, name, produces_artifact):
+    def __init__(self, name, produces_artifact, bazel_produced_artifact):
         """
         name:
           The name of this pom_generation_mode
         produces_artifact:
           Whether this pomgen mode produces a maven artifact
+        bazel_produced_artifact:
+          Whether this pomgen mode represents an artifact that is built by
+          Bazel (ie a jar)
         """
         self.name = name
         self.produces_artifact = produces_artifact
+        self.bazel_produced_artifact = bazel_produced_artifact
 
     def __str__(self):
         return self.name
@@ -42,15 +46,21 @@ class PomGenMode:
 # the pom is generated from scratch, using a common skeleton base template
 # "interesting" pom content is only the <dependencies> section, which is based
 # on BUILD file content
-DYNAMIC = PomGenMode("dynamic", produces_artifact=True)
+DYNAMIC = PomGenMode("dynamic",
+                     produces_artifact=True,
+                     bazel_produced_artifact=True,)
 
 # the pom is generated based on a custom template file only
-TEMPLATE = PomGenMode("template", produces_artifact=True)
+TEMPLATE = PomGenMode("template",
+                      produces_artifact=True,
+                      bazel_produced_artifact=False,)
 
 # this bazel package is skipped over at pom generation time
 # dependencies from this bazel package are "pushed up" to the closest parent
 # that has an artifact producing pom_generation_mode
-SKIP = PomGenMode("skip", produces_artifact=False)
+SKIP = PomGenMode("skip",
+                  produces_artifact=False,
+                  bazel_produced_artifact=False,)
 
 
 DEFAULT = DYNAMIC
